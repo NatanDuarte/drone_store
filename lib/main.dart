84e15d182b2c34773780f028472ae0398171c876
NatanDuarte <natanprocopio@gmail.com>
2022-04-19 15:15:28 -0300

    strings extraction to file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:drone_store/constants.dart';
 import 'package:drone_store/screens/home/home_screen.dart';
+import 'package:drone_store/strings.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,13 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Drone Store',
+      title: appTitle,
       theme: ThemeData(
-        scaffoldBackgroundColor: customBackgroundColor,
-        primaryColor: customPrimaryColor,
-        textTheme: Theme.of(context).textTheme.apply(bodyColor: customTextColor),
-        visualDensity: VisualDensity.adaptivePlatformDensity
-      ),
+          scaffoldBackgroundColor: customBackgroundColor,
+          primaryColor: customPrimaryColor,
+          textTheme:
+              Theme.of(context).textTheme.apply(bodyColor: customTextColor),
+          visualDensity: VisualDensity.adaptivePlatformDensity),
       home: const HomeScreen(),
     );
   }
---
+++ lib/strings.dart
@@ -0,0 +1,1 @@
+const appTitle = 'Drone Store';
