b1d617ccfdf98306db7a9134d4e91d7376debac9
NatanDuarte <natanprocopio@gmail.com>
2022-04-20 11:10:20 -0300

    Refactoring: component header_with_search_bar Extraction

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,5 +1,4 @@
-import 'package:drone_store/constants.dart';
-import 'package:drone_store/strings.dart';
+import 'package:drone_store/screens/home/components/header_with_search_bar.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
@@ -8,68 +7,12 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        SizedBox(
-          height: size.height * 0.15,
-          child: Stack(
-            children: [
-              Container(
-                height: size.height * 0.15 - 25,
-                decoration: const BoxDecoration(
-                  color: customPrimaryColor,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(36),
-                    bottomRight: Radius.circular(36),
-                  ),
-                ),
-                child: Row(
-                  children: [
-                    Text(
-                      'Drones',
-                      style: Theme.of(context).textTheme.headline5?.copyWith(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                    ),
-                  ],
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  margin: const EdgeInsets.symmetric(
-                      horizontal: customDefaultPadding),
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: customDefaultPadding),
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: const Offset(0, 10),
-                        blurRadius: 50,
-                        color: customPrimaryColor.withOpacity(0.23),
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintText: searchBarHint,
-                        hintStyle: TextStyle(
-                          color: customPrimaryColor.withOpacity(0.5),
-                        ),
-                        enabledBorder: InputBorder.none,
-                        focusedBorder: InputBorder.none,
-                        suffixIcon: const Icon(Icons.search)),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        )
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          HeaderWithSearchBar(size: size),
+        ],
+      ),
     );
   }
 }
