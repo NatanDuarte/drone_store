489a53a23f97f198c37e651643ad87d37aca9385
NatanDuarte <natanprocopio@gmail.com>
2022-04-25 16:18:14 -0300

    feat: custom navigation bar created

---
+++ lib/screens/home/components/custom_bottom_navigation_bar.dart
@@ -0,0 +1,41 @@
+import 'package:drone_store/constants.dart';
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  const CustomBottomNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            offset: const Offset(0, -10),
+            blurRadius: 35,
+            color: customPrimaryColor.withOpacity(0.38),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.home),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.star),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.account_circle_outlined),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
